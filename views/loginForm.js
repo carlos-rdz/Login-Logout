22ec53241990d3b4f51ffcfc8cbb4ba62b48bbe6
Carlos Rodriguez <carlos.a.rodriguez100@gmail.com>
2019-02-10 16:54:39 -0500

    adds forgot password redirect

--- views/loginForm.js
+++ views/loginForm.js
@@ -12,8 +12,9 @@ function loginForm(message=""){
     <label for="exampleInputPassword1">Password</label>
     <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
     </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+    <button type="submit" class="btn btn-primary">Submit</button>
     </form>
+    <a href="/forgotpassword">Forgot Password?</a>
     <div class="formMessage text-secondary">${message}</div>
 
 <div>
